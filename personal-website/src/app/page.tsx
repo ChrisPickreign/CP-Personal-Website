fb7104635b57727f2e0536e9f41606b9a54dfc1f
Chris Pickreign <cpickreign21@nobles.edu>
2025-08-19 18:54:16 -0400

    analytics

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -43,7 +43,7 @@ function renderSection(section: string) {
 			return (
 				<section>
 					<h1 className="text-4xl text-blue-500">Chris Pickreign!</h1>
-					<h2>Technical Leader | Software Engineer | Architect</h2>
+					<h2>Software Engineer | Consultant | Founder</h2>
 					<p>
 						Experienced technical leader with 20+ years in software engineering,
 						architecture, and team management. Passionate about building scalable
